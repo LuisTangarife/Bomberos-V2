541bad417fa102890f403bb29eb2dce210c66ab4
LuisTangarife <aux.reg.academicoii@autonoma.edu.co>
2026-06-28 12:24:40 -0500

    Add hover effect to dashboard cards

---
+++ dashboard/dashboard.js
@@ -0,0 +1,11 @@
+document.querySelectorAll(".card").forEach(card=>{
+
+card.addEventListener("mouseenter",()=>{
+
+card.style.transition=".25s";
+
+});
+
+});
+
+console.log("Dashboard cargado correctamente.");
